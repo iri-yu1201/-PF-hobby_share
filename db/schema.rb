ab6fc9c4c5cef3969d72412c1b9595c0a272c5fc
iri-yu1201 <ir12yuta01@gmail.com>
2021-07-07 06:00:25 +0000

    [Create]Postモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_030309) do
+ActiveRecord::Schema.define(version: 2021_07_07_053741) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "detail", null: false
+    t.string "photo"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
