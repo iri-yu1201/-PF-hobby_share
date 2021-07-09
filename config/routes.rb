b40c9895b91337a56b6ca8963aec9a73cc09fc75
iri-yu1201 <ir12yuta01@gmail.com>
2021-07-09 03:08:09 +0000

    [Add]投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'homes#top'
+  resources :users, only:[:show, :edit, :update]
+  resources :posts, only:[:new, :create, :index, :show, :destroy]
 
 end
