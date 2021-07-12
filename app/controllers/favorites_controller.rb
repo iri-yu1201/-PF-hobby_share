7bddb8e6e28b3d1c7bb0cd8131d6be984a84d073
iri-yu1201 <ir12yuta01@gmail.com>
2021-07-12 12:41:19 +0000

    いいね機能追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,14 @@
 class FavoritesController < ApplicationController
   def create
+    @favorite = current_user.favorites.create(item_id: params[:item_id])
+    @favorite.save
+    @item = Item.find(params[:item_id])
   end
-  
+
   def destroy
+    @item = Item.find(params[:item_id])
+    @favorite = current_user.favorites.find_by(item_id: @item.id)
+    @favorite.destroy
   end
-  
+
 end
