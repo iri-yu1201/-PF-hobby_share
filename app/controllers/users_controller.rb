d420a0a4da95eb7ee3beb79c157d9d04165f9291
iri-yu1201 <ir12yuta01@gmail.com>
2021-07-16 14:10:21 +0000

    レイアウト調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
   def show
     @user = User.find(params[:id])
     @items = @user.items.page(params[:page]).reverse_order
