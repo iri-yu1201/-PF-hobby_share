5cfcd0c25c9b93a87acf0c3653f47cb5e371ed2e
iri-yu1201 <ir12yuta01@gmail.com>
2021-07-18 05:10:06 +0000

    検索ページネーション修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
+
 class FavoritesController < ApplicationController
   def create
-    @favorite = current_user.favorites.create(item_id: params[:item_id])
-    @favorite.save
+    @favorite = current_user.favorites.create!(item_id: params[:item_id])
+    @favorite.save!
     @item = Item.find(params[:item_id])
   end
 
   def destroy
     @item = Item.find(params[:item_id])
     @favorite = current_user.favorites.find_by(item_id: @item.id)
-    @favorite.destroy
+    @favorite.destroy!
   end
-
 end
